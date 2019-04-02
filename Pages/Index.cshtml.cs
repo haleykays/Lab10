a31a4126afd791963bb759ef05e62cd3c8a46491
Haley <hksmith2@buffs.wtamu.edu>
2019-04-02 09:21:53 -0500

    new commit

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.Extensions.Logging; 
 namespace Lab10_1.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger _log;
+        public IndexModel(ILogger<IndexModel> log)
+        {
+            _log = log;
+        }
         public void OnGet()
         {
-
+            Object b = null;
+            if(b == null) _log.LogWarning("Object is null!");
+            else b.ToString(); 
         }
     }
 }
--- Pages/NewPage.cshtml.cs
+++ Pages/NewPage.cshtml.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
 
 namespace Lab10_1.Pages
 {
     public class NewPageModel : PageModel
     {
+        private readonly ILogger _log;
+        public NewPageModel(ILogger<NewPageModel> log)
+        {
+            _log = log;
+        }
+        public void OnGet()
+        {
+            Object b = null;
+            if(b == null) _log.LogWarning("Object is null!");
+            else b.ToString(); 
+        }
+        [BindProperty]
+        [Display(Name = "First Name")]
+        public string FirstName {get; set;}
+        [Display(Name = "Last Name")]
+        [BindProperty]
+        public string LastName {get; set;}
         public void OnPost()
         {
-            
+            _log.LogWarning($"First Name: {FirstName} Last Name: {LastName}");           
         }
     }
 }
+
